35203d2c09144b586e66377079cd8d2a9ee49c32
Adir <slutzkis@gmail.com>
2022-09-08 12:32:39 +0300

    Update randomDot.js

--- randomDot.js
+++ randomDot.js
@@ -1,6 +1,13 @@
 function randomDot(){
+    clearDots();
     window.R = Math.floor(Math.random() * 80);
     document.getElementsByClassName("gridDot")[window.R].style.backgroundColor = "red";
 }
 
-function clearDots(){}
+function clearDots(){
+    var x = document.getElementsByClassName("gridDot");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundColor = "tranparent";
+    }
+}
