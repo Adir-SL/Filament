2e751fa298f6c423821c2f04c050db8b7772f71d
Adir <slutzkis@gmail.com>
2022-09-11 13:22:58 +0300

    Update randomDot.js

--- randomDot.js
+++ randomDot.js
@@ -7,13 +7,13 @@ function randomTwice(){
 function randomDot(){
     window.R = Math.floor(Math.random() * 64);
     console.log(window.R);
-    document.getElementsByClassName("gridDot")[window.R].style.backgroundColor = "white";
+    document.getElementsByClassName("gridDot")[window.R].classList.add("selected");
 }
 
 function clearDots(){
     var x = document.getElementsByClassName("gridDot");
     var i;
     for (i = 0; i < x.length; i++) {
-        x[i].style.backgroundColor = "transparent";
+        x[i].classList.remove("selected");
     }
 }
