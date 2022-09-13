0f61c0648c7bf68686088981c6e5371901fcc55b
Adir <slutzkis@gmail.com>
2022-09-13 12:38:18 +0300

    Update randomDot.js

--- randomDot.js
+++ randomDot.js
@@ -14,15 +14,15 @@ function jiggleBoard(){
 function randomDot(){
     if(window.R !== undefined){
         window.PR = window.R;
-        classToAdd = "selected delayAnim";
+        classToAdd = " selected delayAnim";
     }else{
-        classToAdd = "selected";
+        classToAdd = " selected";
     }
     window.R = Math.floor(Math.random() * 64);
     if(window.R == window.PR || window.R == window.PR-1 || window.R == window.PR+1 || window.R == window.PR-8 || window.R == window.PR+8){
         window.R = Math.floor(Math.random() * 64);
     }
-    document.getElementsByClassName("gridDot")[window.R].classList.add(classToAdd);
+    document.getElementsByClassName("gridDot")[window.R].className += classToAdd;
 }
 
 function clearDots(){
