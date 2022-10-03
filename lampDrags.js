9dcb33872619164f25a4addffa553326500b9c55
Adir <slutzkis@gmail.com>
2022-10-03 11:54:02 +0300

    Update lampDrags.js

--- lampDrags.js
+++ lampDrags.js
@@ -1,5 +1,12 @@
 function lampDrag(e){
     // alert(e.target);
+    window.tempLamp = e.target;
+    this.addEventListener('mousemove', dragToMouse(e));
+}
+
+function dragToMouse(e){
+    window.tempLamp.x = e.pageX;
+    window.tempLamp.y = e.pageY;
 }
 
 function lampDrop(e){
