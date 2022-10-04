cd79e0c2389f7c502e0789c191629fb576e9954f
Adir <slutzkis@gmail.com>
2022-10-04 11:46:59 +0300

    Update lampDrags.js

--- lampDrags.js
+++ lampDrags.js
@@ -8,8 +8,10 @@ function lampDrag(e){
 
 function dragMove(e){
     if(window.tempLamp !== undefined){
-        window.tempLamp.style.transform = "translate("+e.pageX - window.tempPageX + "px"+","+e.pageY - window.tempPageY + "px)";
-        console.log(e.pageX-window.tempPageX);
+        window.moveByX = e.pageX - window.tempPageX + "px";
+        window.moveByY = e.pageY - window.tempPageY + "px";
+        window.tempLamp.style.transform = "translate("+e.pageX - window.tempPageX + "px,"+e.pageY - window.tempPageY + "px)";
+        console.log(window.moveByX + "," + window.moveByY);
     }
 }
 
