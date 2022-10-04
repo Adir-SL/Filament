195751f24251e1fabbe127e5530fbee47ae136e7
Adir <slutzkis@gmail.com>
2022-10-04 11:44:49 +0300

    Update lampDrags.js

--- lampDrags.js
+++ lampDrags.js
@@ -1,14 +1,16 @@
 function lampDrag(e){
     // alert(e.target);
-    window.tempLamp = e.target;
+    window.tempLamp = e.target.parentElement;
     window.tempPageX = e.pageX;
     window.tempPageY = e.pageY;
     // addEventListener('mousemove', dragToMouse(e));
 }
 
 function dragMove(e){
-    e.target.style.transform = "translate("+e.pageX - window.tempPageX + "px"+","+e.pageY - window.tempPageY + "px)";
-    console.log("move")
+    if(window.tempLamp !== undefined){
+        window.tempLamp.style.transform = "translate("+e.pageX - window.tempPageX + "px"+","+e.pageY - window.tempPageY + "px)";
+        console.log(e.pageX-window.tempPageX);
+    }
 }
 
 function lampDrop(e){
